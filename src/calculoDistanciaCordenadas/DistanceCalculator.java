13b0dc0187f3108a15cc22ba19f807af57879a7f
michelzarpe <michelzarpe@gmail.com>
2018-09-08 11:01:57 -0300

    Classes funcionando

--- src/calculoDistanciaCordenadas/DistanceCalculator.java
+++ src/calculoDistanciaCordenadas/DistanceCalculator.java
@@ -1,50 +1,19 @@
 package calculoDistanciaCordenadas;
 
-/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-/*::                                                                         :*/
-/*::  This routine calculates the distance between two points (given the     :*/
-/*::  latitude/longitude of those points). It is being used to calculate     :*/
-/*::  the distance between two locations using GeoDataSource (TM) prodducts  :*/
-/*::                                                                         :*/
-/*::  Definitions:                                                           :*/
-/*::    South latitudes are negative, east longitudes are positive           :*/
-/*::                                                                         :*/
-/*::  Passed to function:                                                    :*/
-/*::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :*/
-/*::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :*/
-/*::    unit = the unit you desire for results                               :*/
-/*::           where: 'M' is statute miles (default)                         :*/
-/*::                  'K' is kilometers                                      :*/
-/*::                  'N' is nautical miles                                  :*/
-/*::  Worldwide cities and other features databases with latitude longitude  :*/
-/*::  are available at https://www.geodatasource.com                          :*/
-/*::                                                                         :*/
-/*::  For enquiries, please contact sales@geodatasource.com                  :*/
-/*::                                                                         :*/
-/*::  Official Web site: https://www.geodatasource.com                        :*/
-/*::                                                                         :*/
-/*::           GeoDataSource.com (C) All Rights Reserved 2017                :*/
-/*::                                                                         :*/
-/*::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
 class DistanceCalculator
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, "M") + " Miles\n");
-		System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, "K") + " Kilometers\n");
-		System.out.println(distance(32.9697, -96.80322, 29.46786, -98.53506, "N") + " Nautical Miles\n");
+		System.out.println(distance(-27.176446,-51.5140078, -27.1345282, -52.6015737, "K") + " Kilometers\n");	
 	}
 
 	private static double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
 		double theta = lon1 - lon2;
+		
 		double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
 		dist = Math.acos(dist);
 		dist = rad2deg(dist);
+	
 		dist = dist * 60 * 1.1515;
 		if (unit == "K") {
 			dist = dist * 1.609344;
